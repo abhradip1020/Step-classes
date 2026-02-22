c743f17ca471a3bf174dd304893d674ef24547d3
abhradip1020 <abhradip1020@gmail.com>
2026-02-22 17:58:25 +0530

    UC2.java

--- UC2.java
+++ UC2.java
@@ -2,7 +2,7 @@
  * @author Abhradip
 * 
 */
-
+import java.util.*;
 public class UC2 {
     public static void main(String args[]){
         System.out.println("   *        *      **          ** ");
